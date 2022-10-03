bf3703444960b27051db746cc4a716886376d97c
Nobuchika Tanaka <sleepingdog.7799@gmail.com>
2022-10-03 17:44:34 +0900

    Refactor Repeat()

--- repeat.go
+++ repeat.go
@@ -1,11 +1,13 @@
 package iteration
 
+const repeatCount = 5
+
 // Repeat returns character repeated 5 times.
 func Repeat(character string) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 	}
 
 	return repeated
